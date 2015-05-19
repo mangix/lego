1cf99ca59b5f317c0baf0d74330ad616e2c41f59
mangix <maqh1988@gmail.com>
2015-05-19 22:49:13 +0800

    test lego.pipe return this

--- test/lego.js
+++ test/lego.js
@@ -6,7 +6,11 @@ var Lego = require("../lib/lego");
 
 describe("lego.Lego", function () {
     describe("Lego.start" , function(){
-
+        it("should return this" , function(){
+            var lego = new Lego();
+            var params = {};
+            expect(lego.start(params)).to.equal(lego);
+        });
 
         it("should set params to this.params" , function(){
             var lego = new Lego();
@@ -14,11 +18,12 @@ describe("lego.Lego", function () {
             lego.start(params);
             expect(lego.params).to.equal(params);
         });
+    });
 
+    describe("Lego.pipe" , function(){
         it("should return this" , function(){
             var lego = new Lego();
-            var params = {};
-            expect(lego.start(params)).to.equal(lego);
+            expect(lego.pipe()).to.equal(lego);
         });
-    })
+    });
 });
