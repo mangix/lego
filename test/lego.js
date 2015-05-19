3e6ebb3d072cc6beda5d02567e20ca3be61f4a71
mangix <maqh1988@gmail.com>
2015-05-19 22:40:10 +0800

    test lego.start

--- test/lego.js
+++ test/lego.js
@@ -5,5 +5,12 @@ var expect = require("chai").expect;
 var Lego = require("../lib/lego");
 
 describe("lego.Lego", function () {
-
+    describe("Lego.start" , function(){
+        it("should set params to this.params" , function(){
+            var lego = new Lego();
+            var params = {};
+            lego.start(params);
+            expect(lego.params).to.equal(params);
+        })
+    })
 });
